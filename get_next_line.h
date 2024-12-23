1bf87e3b59239a25b348cfb7bdd7f032ea18f65b
Xidruk <xidruk@gmail.com>
2024-12-23 08:58:52 +0100

    get_next_line.h header file blabla

--- get_next_line.h
+++ get_next_line.h
@@ -1,17 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbarkan <kbarkan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 17:10:24 by kbarkan           #+#    #+#             */
+/*   Updated: 2024/12/08 17:19:34 by kbarkan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 69
+#  define BUFFER_SIZE INT_MAX
 # endif
 
-char    *get_next_line(int fd);
-size_t get_length(const char *string);
-char *join_strings(char *s1,  char *s2);
-char *c_search(const char *s_in_str, int c);
-char *duplicate_string(const char *or_str);
+char	*get_next_line(int fd);
+size_t	get_length(const char *string);
+char	*join_strings(char *s1, char *s2);
+char	*c_search(const char *s_in_str, int c);
+char	*duplicate_string(const char *or_str);
 
 #endif
